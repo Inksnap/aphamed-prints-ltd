11b4dc579d04f2266de23201024a0b1770c47355
Beautifulone0 <konyeanifaith@gmail.com>
2025-12-14 19:13:03 +0100

    Added chatbox and whatsapp

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import Navbar from "./components/navbar.jsx";
 import Preloader from "./components/Preloader.jsx";
 import Footertwo from "./components/footertwo.jsx";
+import WhatsAppButton from "./components/WhatsAppButton.jsx";
+import ChatWidget from "./components/ChatWidget.jsx";
 
 export const metadata = {
   title: "Aphamed Prints LTD",
@@ -43,6 +45,8 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main>{children}</main>
         <Footertwo />
+        <WhatsAppButton />
+        <ChatWidget />
         </div>
         
       </body>
